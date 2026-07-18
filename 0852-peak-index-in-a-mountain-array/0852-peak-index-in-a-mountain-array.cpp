f0fdd1bbb50aba3a79bbaab9d9da6b915b2e9b1c
Ansh Upadhayay <197792323+anshu706@users.noreply.github.com>
2026-07-18 15:18:25 +0530

    Time: 0 ms (100%), Space: 63.5 MB (79.03%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -6,17 +6,14 @@ public:
         while(st <= end) {
             int mid = st + (end - st) / 2;
 
-            // Found the peak
             if(arr[mid-1] < arr[mid] && arr[mid] > arr[mid+1]) {
                 return mid;
             }
-            // On descending slope -> peak is to the left
             else if(arr[mid+1] < arr[mid]) {
-                end = mid - 1; // FIXED: shifted from st = mid + 1
+                end = mid - 1;
             }
-            // On ascending slope -> peak is to the right
             else {
-                st = mid + 1;  // FIXED: shifted from end = mid - 1
+                st = mid + 1;
             }
         }
         return -1;
